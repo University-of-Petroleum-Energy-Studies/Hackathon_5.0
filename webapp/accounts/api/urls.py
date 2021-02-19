a314792c5483faee9a6d86c52f4a610f3961067e
tejas-trivedi <500077185@stu.upes.ac.in>
2021-02-19 21:05:11 +0530

    auth

---
+++ webapp/accounts/api/urls.py
@@ -0,0 +1,24 @@
+from django.urls import include, path
+from .views import (
+    TraderRegisterView,
+    FarmerRegisterView,
+    UserLoginView,
+    UserLogoutView,
+    ValidateOTP,
+    ValidatePhoneNumberSendOTP,
+)
+
+urlpatterns = [
+    path("send/otp/", ValidatePhoneNumberSendOTP.as_view(), name="send_otp"),
+    path("verify/otp/", ValidateOTP.as_view(), name="verify_otp"),
+    path(
+        "register/users/traders/",
+        TraderRegisterView.as_view(),
+        name="register_traders",
+    ),
+    path(
+        "register/users/farmers/", FarmerRegisterView.as_view(), name="register_farmers"
+    ),
+    path("login/", UserLoginView.as_view(), name="user_login"),
+    path("logout/", UserLogoutView.as_view(), name="user_logout"),
+]
---
+++ webapp/accounts/forms.py
@@ -0,0 +1,75 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+from .models import User
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("phone_number",)
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get("phone_number")
+        qs = User.objects.filter(phone_number=phone_number)
+        if qs.exists():
+            raise forms.ValidationError("phone_number is taken")
+        return phone_number
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+
+class UserAdminCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ("phone_number",)
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UserAdminCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserAdminChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = (
+            "phone_number",
+            "password",
+            "is_active",
+            "is_superuser",
+            "is_staff",
+        )
+
+    def clean_password(self):
+
+        return self.initial["password"]
+                  
