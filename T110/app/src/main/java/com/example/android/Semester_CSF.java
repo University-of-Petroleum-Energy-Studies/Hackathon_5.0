8abb40c93c2b5b2e64ed33efe8fcefdb53ec02b6
sakshamlamba109 <500075568@stu.upes.ac.in>
2021-02-20 14:08:48 +0530

    hackathon 5.0

---
+++ T110/app/src/main/java/com/example/android/Semester_CSF.java
@@ -0,0 +1,14 @@
+package com.example.android;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Semester_CSF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semester__c_s_f);
+    }
+}
