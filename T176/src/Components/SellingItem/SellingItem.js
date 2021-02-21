06714154bee8380461bfe06298f4fd671a9025b0
Pranjal Srivastava <42092291+pranjals149@users.noreply.github.com>
2021-02-21 18:59:00 +0530

    T176

--- T176/src/Components/SellingItem/SellingItem.js
+++ T176/src/Components/SellingItem/SellingItem.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import { useStateValue } from '../../StateProvider';
 import './SellingItem.css'
 
+
 const SellingItem = () => {
+
+    const [{ user }, dispatch] = useStateValue();
+    const history = useHistory();
+
     return (
         <div className='sellingItem'>
-            <h1>Congratulations, your item is saved with us </h1>
+            <h2 style={{ color: 'black', fontWeight: '600', letterSpacing: '4px' }}>Congratulations <strong>{user?.email}</strong> your item is saved with us </h2>
+
+            <button className='sellingItem__button' onClick={() => history.push('/sell')}>Go Back to Sell Page</button>
+
+            <button className='sellingItem__button' onClick={() => history.push('/buyorsell')}>Go Back to Main Page</button>
         </div>
     )
 }
