d4bff3092d5008fa69f7364225394a0c7cb32914
Jigyanshu R <48449428+iamjr15@users.noreply.github.com>
2021-03-02 15:41:35 +0530

    Delete T165/T165-shagun-commit 2 directory

--- T165/T165-shagun-commit 2/android/app/src/main/kotlin/com/example/WSHCRD/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.WSHCRD
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- T165/T165-shagun-commit 2/lib/firebase_services/basic_firebase.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-FirebaseAuth authInstance = FirebaseAuth.instance;
-final db = Firestore.instance;
